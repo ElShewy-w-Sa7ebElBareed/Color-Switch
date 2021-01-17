6b5cbe0c3ae214bdfd6c207bcf1e65edaff5efca
ahmed <ahmed@192.168.1.7>
2021-01-17 17:35:04 +0200

    star class

---
+++ Color Switch/src/application/Shapes/StarShape.java
@@ -0,0 +1,19 @@
+package application.Shapes;
+
+import javafx.scene.shape.Shape;
+
+public class StarShape extends AppShape {
+
+	@Override
+	public void Move(double stageStep) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Shape[] getShape() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
